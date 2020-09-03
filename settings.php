1d7453006c05e5ae4acaca3627c00715786a75bc
VincentSD <vidonkoh@gmail.com>
2020-09-03 19:43:11 +0000

    Delete settings.php

--- settings.php
+++
@@ -1,7 +0,0 @@
-<?php
-if ($ADMIN->fulltree) {
-    $settings->add(new admin_setting_configcheckbox('block_payments/showcourses',
-                   get_string('showcourses', 'block_payments'),
-                   get_string('showcoursesdesc', 'block_payments'),
-                   0));
-}
